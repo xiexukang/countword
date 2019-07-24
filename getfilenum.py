40343c1c74d3254e0f7715b9d04b92a13f0dcbe6
码上有云 <18861872581@163.com>
2019-07-24 11:42:00 +0800

    新增层次遍历代码

--- getfilenum.py
+++ getfilenum.py
@@ -1,6 +1,7 @@
 import os
 
 Directorylist = []
+#递归方式
 def FileList(dir,fileList):
     newDir = dir
     if os.path.isfile(dir):   #判断是文件？是文件表示没有子文件
@@ -9,10 +10,39 @@ def FileList(dir,fileList):
         Directorylist.append(dir)
         for s in os.listdir(dir):
             newDir = os.path.join(dir,s)
+            print('测试',newDir)
             FileList(newDir,fileList)  #递归检索子文件夹
     return fileList
+#使用层次遍历
+j=0
+def GetFile(dir):
+    list2=[]
+    list1=os.listdir(dir)
+    l =len(list1)
+    m=0
+    while m<l:
+        list1[m]=os.path.join(dir,list1[m])#使其子目录具有完整路径
+        m +=1
+    #用于测试，调试bug,print('初始长度',l)print('初始文件目录:',list1)
+    for i in list1:
+
+        newdir = i
+        #print('测试1：',newdir)
+        if os.path.isfile(newdir):
+            list2.append(newdir)
+            #测试：print('附加后的文件:',list1)
+        else:
+
+            for k in os.listdir(newdir):
+                list1.append(os.path.join(newdir,k))
+            #print('附加后的目录:',list1)
+    return list2
+n = input("输入你要使用的方式.1：递归检测，2：层次遍历检测")
 File = input('输入你要检测的文件地址:')
-fileList =FileList('%s'% File,[])
+if(int(n)==1):
+    fileList = FileList('%s' % File, [])
+else:
+    fileList = GetFile('%s'% File)
 for f in fileList:
     print(f)
 
